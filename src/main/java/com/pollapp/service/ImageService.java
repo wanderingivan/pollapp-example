b90e4ca0d0a8476743edeca817f0559b0c640132
wanderingivan <angushev.ivan@gmail.com>
2017-06-02 14:14:38 +0300

    Add ImageService - service layer interface that provides methos for interacting with images stored on fs.

---
+++ src/main/java/com/pollapp/service/ImageService.java
@@ -0,0 +1,43 @@
+package com.pollapp.service;
+
+import java.io.File;
+import java.io.IOException;
+
+
+/**
+ * 
+ * Service Layer interface providing
+ * basic save and retrieve operations
+ * for images.
+ * @throws IOException when missing files
+ * or lacking credentials to save in directories 
+ */
+public interface ImageService {
+
+	/**
+	 * Retrieve an image as a base 64 string
+	 * @param name of the file to use
+	 * @return an image converted to a base 64 string
+	 * @throws IOException
+	 */
+	String getB64(String name);
+	
+	/**
+	 * Retrieve an image as a byte[]
+	 * @param name the name of the image.<br/>
+	 * This should not have to include the folder location!
+	 * @return image converted as byte[]
+	 */
+	File loadImage(String name); 
+	
+	
+	
+	/**
+	 * Saves an image to a predefined destination
+	 * @param file
+	 * @param contentType
+	 * @param fileName
+	 * @throws IOException
+	 */
+	String saveImage(File file,String contentType,String fileName) throws IOException;
+}
